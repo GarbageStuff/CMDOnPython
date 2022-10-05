2e8cb66ce8340acfca35e163b7b92333c2e90295
Alan Inter <90340994+CDh8u@users.noreply.github.com>
2022-10-05 21:04:52 +0300

    main script

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+import os
+print("hello")
+print("this will automatically open cmd with python?")
+time.sleep(10)
+print("give it more time")
+print("made by cdh8u with basic python")
+time.sleep(5)
+os.system("cmd.exe")
